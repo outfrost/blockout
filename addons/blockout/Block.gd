4ba579de651aee30115e1791876a6cd02360fe17
outfrost <kotlet.bahn@gmail.com>
2022-12-10 02:16:30 +0100

    Give Block a resizing CubeMesh

--- addons/blockout/Block.gd
+++ addons/blockout/Block.gd
@@ -1,14 +1,23 @@
 tool
 extends Spatial
 
+const MATERIAL: Material = preload("res://addons/blockout/material/block_mat.tres")
+
 export var size: = Vector3.ONE setget set_size
 
-func _ready() -> void:
-	pass
+onready var mesh_inst: MeshInstance = $"%MeshInstance"
 
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	if BlockoutUtil.plugin.get_editor_interface().get_edited_scene_root() == self:
+		return
+
+	var mesh: = CubeMesh.new()
+	mesh.size = size
+	mesh.material = MATERIAL
+	mesh_inst.mesh = mesh
 
 func set_size(v: Vector3) -> void:
 	size = v
+	if mesh_inst.mesh:
+		mesh_inst.mesh.size = size
 	update_gizmo()
---
+++ addons/blockout/BlockoutUtil.gd
@@ -0,0 +1,4 @@
+tool
+extends Node
+
+var plugin: EditorPlugin
